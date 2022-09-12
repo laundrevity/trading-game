009442d361043cdf0dc9e4991fb59685784099d1
Conor Mahany <conor.mahany@maverickderivatives.com>
2022-09-12 17:51:09 +0200

    added open.html

--- python/main.py
+++ python/main.py
@@ -13,6 +13,7 @@ app = socketio.ASGIApp(sio, quart_app)
 usm = UnixSocketManager(sio)
 gm = GameManager(sio)
 
+
 async def tick():
     while True:
         data = {
@@ -46,9 +47,16 @@ async def width():
     # here is where the opening width is established, and initial bid/ask is established
     return await render_template('width.html', foo="foo")
 
+
 @quart_app.route('/market')
 async def market():
     return await render_template('market.html')
+
+
+@quart_app.route('/open')
+async def open_route():
+    return await render_template('open.html')
+
 
 @sio.on('connect')
 async def connect(sid, environ):
@@ -57,11 +65,13 @@ async def connect(sid, environ):
     if sid not in gm.players:
         gm.register_player(sid)
 
+
 @sio.on('register_browser_connection')
 async def register_browser_connection(sid, msg):
     print(f"register_browser_connection with {sid=}, {msg=}", flush=True)
     j = json.loads(msg)
     gm.browser_ids[sid] = j['browser_session_id']
+
 
 @sio.event
 async def handle_width_update(sid, msg):
@@ -78,6 +88,7 @@ async def handle_width_update(sid, msg):
     else:
         print(f"gm.current_width_game is None", flush=True)
 
+
 @sio.event
 async def handle_bid_submission(sid, msg):
     print(f"handle_bid_submission for {msg=} from {sid=}", flush=True)
@@ -91,6 +102,16 @@ async def handle_bid_submission(sid, msg):
     # redirect to trading open
     await sio.emit("advance_to_trading_open", json.dumps(payload), broadcast=True)
     # wait a second for folks to get redirected
+    await asyncio.sleep(1)
+    # broadcast the initial book
+    await sio.emit("book", gm.get_book_json(), broadcast=True)
+
+
+@sio.event
+async def handle_open_side(sid, msg):
+    print(f"handle_open_side for {msg=} from {sid=}", flush=True)
+    data = json.loads(msg)
+    await sio.emit("advance_to_market", json.dumps({}))
     await asyncio.sleep(1)
     # broadcast the initial book
     await sio.emit("book", gm.get_book_json(), broadcast=True)
